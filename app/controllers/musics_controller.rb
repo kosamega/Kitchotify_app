b0a8d9bcdbdf967167ea63a6da732c71bfce0215
kosamega <kitchonkosamega@gmail.com>
2022-08-20 18:19:15 +0900

    m

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,7 +1,15 @@
 class MusicsController < ApplicationController
     before_action :logged_in_user
+    
     def show
         @music = Music.find_by(id: params[:id])
         @comments = @music.comments
+        require 'aws-sdk-s3'
+        s3 = Aws::S3::Client.new
+        signer = Aws::S3::Presigner.new(client: s3)
+        @url = signer.presigned_url(:get_object, 
+                                    bucket: 'kitchotifyappstrage',
+                                    key: "audio/#{@music.audio_path}",
+                                    expires_in: 10)
     end
 end
