a9a9d43b4cf17167b4c41f17d7df74656bcc25fb
kosamega <kitchonkosamega@gmail.com>
2022-09-07 20:38:33 +0900

    make better

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,10 +18,10 @@ module SessionsHelper
         @current_user ||= User.find_by(id: user_id)
         elsif (user_id = cookies.encrypted[:user_id])
         user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
-            log_in user
-            @current_user = user
-        end
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
 
--- test/controllers/music_playlist_relations_controller_test.rb
+++ test/controllers/music_playlist_relations_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class MusicPlaylistRelationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:kosamega)
+    @other_user = users(:posamega)
+    @playlist = playlists(:one)
+  end
+
+  test "持ち主はプレイリストに曲を追加できる" do
+    log_in_as(@user)
+    assert_difference 'MusicPlaylistRelation.count', 1 do
+      post music_playlist_relations_path, params: { music_id: 1, playlist_id: 1 }
+    end
+  end
+
+  test "持ち主以外はプレイリストに曲を追加できない" do
+    log_in_as(@other_user)
+    assert_no_difference 'MusicPlaylistRelation.count' do
+      post music_playlist_relations_path, params: { music_id: 1, playlist_id: 1 }
+    end
+  end
 end
