9e83778c52bc22394545897a3d742312d0e4a0ff
kosamega <kitchonkosamega@gmail.com>
2022-09-13 23:45:03 +0900

    fix

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,5 @@ class Like < ApplicationRecord
     belongs_to :user
     belongs_to :music
     default_scope -> { order(created_at: :desc) }
+    validates :music_id, uniqueness: { scope: :user_id } 
 end
