f23d2e3ca0f8c4710627b3f6d1ce8d61ac29beba
kosamega <kitchonkosamega@gmail.com>
2022-09-14 17:20:15 +0900

    fix

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,4 +3,5 @@ class Playlist < ApplicationRecord
     has_many :music_playlist_relations, dependent: :destroy
     has_many :musics_included, through: :music_playlist_relations, source: :music
     validates :name, presence: true
+    default_scope -> { order(id: :desc) }
 end
