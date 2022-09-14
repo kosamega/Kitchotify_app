fcf67730ac0fd1aad33320c18291f11659d1662a
kosamega <kitchonkosamega@gmail.com>
2022-09-14 16:49:50 +0900

    fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
     before_action :logged_in_user, :not_kitchonkun
     def create
-        comment = Comment.new(content: params[:comment][:content], user_id: current_user.id, music_id: params[:music_id])
-        comment.save
-        redirect_to music_path(params[:music_id])
+        @comment = Comment.new(content: params[:comment][:content], user_id: current_user.id, music_id: params[:music_id])
+        @comment.save
+        respond_to do |format|
+            format.html {redirect_back_or "/"}
+            format.js
+        end      
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_142554) do
+ActiveRecord::Schema.define(version: 2022_09_14_063305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2022_09_13_142554) do
     t.datetime "updated_at", precision: 6
     t.text "bio"
     t.text "remember_digest"
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
