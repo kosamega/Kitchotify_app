a742df99611e4e320708e51f383be1e80fff4c7a
kosamega <kitchonkosamega@gmail.com>
2022-12-11 23:55:03 +0900

    a

--- test/controllers/intro_quizzes_controller_test.rb
+++ test/controllers/intro_quizzes_controller_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class IntroQuizzesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @intro_quiz = intro_quiz(:one)
+  end 
+
   test "should get show" do
-    get intro_quizzes_show_url
+    get intro_quiz_path(@intro_quiz)
     assert_response :success
   end
 
   test "should get index" do
-    get intro_quizzes_index_url
+    get intro_quizzes_path
     assert_response :success
   end
 end
