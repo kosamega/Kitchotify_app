a006721bc3acb16874e2497f95c192a9b1ba481c
kosamega <kitchonkosamega@gmail.com>
2022-12-12 23:45:04 +0900

    music_playlist_relations_controller_test.rbができない

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -3,34 +3,35 @@ require "test_helper"
 class LikesControllerTest < ActionDispatch::IntegrationTest
     def setup
         @user = users(:kosamega)
-        @like = likes(:one)
+        @music = musics(:waopo)
     end
 
     test "ユーザーが有効" do
         assert @user.valid?
     end
 
-    test "likeが有効" do
-        assert @like.valid?
-    end
-
     test "ログインしないとlikeを作成出来なく、するとできる" do
-        post likes_path, params: {music_id: 1, user_id: 1}
+        post likes_path, params: {music_id: @music.id, user_id: @user.id}
         assert_not flash.empty?
         assert_redirected_to "/login"
         log_in_as(@user)
         assert_difference "Like.count", 1 do
-            post likes_path, params: {music_id: 1, user_id: 1}
+            post likes_path, params: {music_id: @music.id, user_id: @user.id}
         end
     end
     
     test "ログインしないとlikeを削除できなく、するとできる" do
-        delete like_path(@like)
+        log_in_as(@user)
+        post likes_path, params: {music_id: @music.id, user_id: @user.id}
+        like = Like.find_by(music_id: @music.id, user_id: @user.id)
+        assert_difference "Like.count", -1 do
+            delete like_path(like), params: {like_id: like.id}
+        end
+        delete "/logout"
+        assert_not is_logged_in?
+        delete like_path(like), params: {like_id: like.id}
         assert_not flash.empty?
         assert_redirected_to "/login"
-        assert_difference "Like.count", -1 do
-            delete like_path(@like)
-        end
     end
     
     test "ログインしないとlike一覧にアクセスできない" do
