d3816b0cb2307bdd33e9c663a31643c1ff0d524b
kosamega <kitchonkosamega@gmail.com>
2022-12-14 21:36:28 +0900

    finish

--- app/controllers/intro_quizzes_controller.rb
+++ app/controllers/intro_quizzes_controller.rb
@@ -1,5 +1,6 @@
 class IntroQuizzesController < ApplicationController
   before_action :logged_in_user
+  include MusicsHelper
 
   def show
     @playlists = current_user.playlists
@@ -10,19 +11,8 @@ class IntroQuizzesController < ApplicationController
       @musics_all = Music.all
     end
     @infos = []
-    @musics = []
-    require 'aws-sdk-s3'
-    s3 = Aws::S3::Client.new
-    signer = Aws::S3::Presigner.new(client: s3)
-    @ids.each do |id|
-      music = Music.find_by(id: id)
-      url = signer.presigned_url(:get_object, 
-                                  bucket: 'kitchotifyappstrage',
-                                  key: "audio/#{music.album.id}/#{music.audio_path}",
-                                  expires_in: 7200)
-      @infos.push({url: url, name: music.name, artist: music.artist})
-      @musics.push(music)
-    end
+    @musics = @ids.map{ |id| Music.find_by(id: id) }
+    set_infos(@musics)
     gon.infos_j = @infos
     gon.ids = @ids
     gon.q_num = @q_num
--- app/helpers/musics_helper.rb
+++ app/helpers/musics_helper.rb
@@ -1,2 +1,18 @@
 module MusicsHelper
+  def set_infos(musics)
+    require 'aws-sdk-s3'
+    s3 = Aws::S3::Client.new
+    signer = Aws::S3::Presigner.new(client: s3)
+    musics.each do |music|
+      if music.audio.attached?
+        url = music.audio.url
+      else
+        url = signer.presigned_url(:get_object, 
+                                    bucket: 'kitchotifyappstrage',
+                                    key: "audio/#{music.album.id}/#{music.audio_path}",
+                                    expires_in: 7200)
+      end
+      @infos.push({url: url, name: music.name, artist: music.artist})
+    end
+  end
 end
