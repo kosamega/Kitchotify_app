964625ed01fb7ca69b9a93a574ee85a3e7d6cb44
kosamega <kitchonkosamega@gmail.com>
2022-12-17 02:30:56 +0900

    a

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
     has_many :musics, class_name: "Music", foreign_key: "album_id"
     has_one_attached :jacket
-    default_scope -> { order(id: :desc) }
+    default_scope -> { order(kiki_taikai_date: :asc) }
 end
