5ca0fcc85e99990777489f07cc85664cef8322ab
kosamega <kitchonkosamega@gmail.com>
2022-12-17 00:23:15 +0900

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_131600) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_151045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_131600) do
     t.datetime "updated_at", null: false
     t.integer "editor_id"
     t.boolean "released", default: false
-    t.date "kiki_taikai_yr_mo"
+    t.date "kiki_taikai_date"
   end
 
   create_table "comments", force: :cascade do |t|
