89a4a3af965e21f8052eaf81d33d7b30a4b5f558
kosamega <kitchonkosamega@gmail.com>
2022-12-17 00:23:15 +0900

    finish

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -42,6 +42,6 @@ class AlbumsController < ApplicationController
 
     private 
         def album_params
-            params.require(:album).permit(:name, :jacket)
+            params.require(:album).permit(:name, :jacket, :kiki_taikai_date, :released)
         end
 end
