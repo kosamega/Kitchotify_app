f3e7446538837cebf2e559339d3bed35fca1db1e
kosamega <kitchonkosamega@gmail.com>
2022-12-26 00:06:31 +0900

    a

--- app/helpers/musics_helper.rb
+++ app/helpers/musics_helper.rb
@@ -1,18 +1,7 @@
 module MusicsHelper
   def set_infos(musics)
-    require 'aws-sdk-s3'
-    s3 = Aws::S3::Client.new
-    signer = Aws::S3::Presigner.new(client: s3)
     musics.each do |music|
-      if music.audio.attached?
-        url = music.audio.url
-      else
-        url = signer.presigned_url(:get_object, 
-                                    bucket: 'kitchotifyappstrage',
-                                    key: "audio/#{music.album.id}/#{music.audio_path}",
-                                    expires_in: 7200)
-      end
-      @infos.push({url: url, name: music.name, artist: music.artist, index_info: music.index_info})
+      @infos.push({url: music.audio.url, name: music.name, artist: music.artist, index_info: music.index_info})
     end
   end
 end
