644654642d76cfeddcff2524509d5847317154b4
kosamega <kitchonkosamega@gmail.com>
2022-12-26 00:25:16 +0900

    a

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :music
-    default_scope -> { order(created_at: :desc) }
-    validates :music_id, uniqueness: { scope: :user_id } 
+  belongs_to :user
+  belongs_to :music
+  default_scope -> { order(created_at: :desc) }
+  validates :music_id, uniqueness: { scope: :user_id }
 end
--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -1,31 +1,31 @@
-require "test_helper"
+require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:kosamega)
   end
 
-  test "無効なユーザー登録は失敗する" do
+  test '無効なユーザー登録は失敗する' do
     log_in_as(@user)
     get new_user_path
-    assert_no_difference "User.count" do
-      post users_path, params: { user: { name: "",
-                                         password: "foo",
-                                         password_confirmation: "bar" } }
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
     end
     follow_redirect!
-    assert_template "users/new"
+    assert_template 'users/new'
   end
 
-  test "有効なユーザー登録は成功する" do
+  test '有効なユーザー登録は成功する' do
     log_in_as(@user)
     get new_user_path
-    assert_difference "User.count", 1 do
-      post users_path, params: { user: { name: "example",
-                                         password: "password",
-                                         password_confirmation: "password" } }
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'example',
+                                         password: 'password',
+                                         password_confirmation: 'password' } }
     end
     follow_redirect!
-    assert_template "users/new"
+    assert_template 'users/new'
   end
 end
