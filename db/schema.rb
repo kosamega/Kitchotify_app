2a90781ee6de764e8054321830b08111fdf5c862
kosamega <kitchonkosamega@gmail.com>
2023-01-10 17:56:13 +0900

    usersからemailを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_07_092930) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_085348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,7 +114,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_092930) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
