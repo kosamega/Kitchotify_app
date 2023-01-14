b38d1149e806cc3ed6b310cca53789487c639ecf
kosamega <kitchonkosamega@gmail.com>
2023-01-14 22:42:01 +0900

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_122335) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_133751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_122335) do
     t.integer "editor_id"
     t.boolean "released", default: false
     t.date "kiki_taikai_date"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
