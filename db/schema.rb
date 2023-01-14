eb0c9b440a9b960f171f8f3c9f1c84be48b001a5
kosamega <kitchonkosamega@gmail.com>
2023-01-14 22:06:25 +0900

    albumのnameをnot nullに修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_10_125906) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_122335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_125906) do
   end
 
   create_table "albums", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "editor_id"
