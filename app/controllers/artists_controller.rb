0783e8bf74df7f285af65b793f09eaf3202ce5a6
kosamega <kitchonkosamega@gmail.com>
2023-01-14 22:54:58 +0900

    fix

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,49 +1,38 @@
 class ArtistsController < ApplicationController
+  before_action :logged_in_user
   before_action :set_artist, only: %i[ show edit update destroy ]
 
-  # GET /artists or /artists.json
   def index
     @artists = Artist.all
   end
 
-  # GET /artists/1 or /artists/1.json
   def show
   end
 
-  # GET /artists/new
   def new
     @artist = Artist.new
   end
 
-  # GET /artists/1/edit
   def edit
   end
 
-  # POST /artists or /artists.json
   def create
     @artist = Artist.new(artist_params)
-
-    respond_to do |format|
-      if @artist.save
-        format.html { redirect_to artist_url(@artist), notice: "Artist was successfully created." }
-        format.json { render :show, status: :created, location: @artist }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @artist.errors, status: :unprocessable_entity }
-      end
+    if @artist.save
+      flash[:success] = "#{@artist}を作成しました"
+      redirect_to artist_path(@artist)
+    else
+      flash[:danger] = @artist.errors.full_messages.join('<br>')
+      render 'new'
     end
   end
 
-  # PATCH/PUT /artists/1 or /artists/1.json
   def update
-    respond_to do |format|
-      if @artist.update(artist_params)
-        format.html { redirect_to artist_url(@artist), notice: "Artist was successfully updated." }
-        format.json { render :show, status: :ok, location: @artist }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @artist.errors, status: :unprocessable_entity }
-      end
+    if @artist.update(artist_params)
+      flash[:success] = "更新しました"
+      redirect_to artist_path(@artist)
+    else
+      flash[:danger] = @artist.errors.full_messages.join('<br>')
     end
   end
 
