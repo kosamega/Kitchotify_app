d37277eb5905cc664d544d03eb855add34e8da59
kosamega <kitchonkosamega@gmail.com>
2023-01-20 14:41:14 +0900

    rubocop

--- test/integration/musics_create_test.rb
+++ test/integration/musics_create_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class MusicsCreateTest < ActionDispatch::IntegrationTest
   def setup
@@ -9,8 +9,8 @@ class MusicsCreateTest < ActionDispatch::IntegrationTest
   test 'musicを作成できる' do
     get new_album_music_path(@album)
     assert_difference 'Music.count', 1 do
-      post album_musics_path(@album), params: {music: {name: "new_music",
-                                                                  artist_name: @artist.name}}
+      post album_musics_path(@album), params: { music: { name: 'new_music',
+                                                         artist_name: @artist.name } }
     end
     assert flash.any?
     assert_template 'musics/new'
