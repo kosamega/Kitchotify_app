3bb824b2357794d20e403209e327fb5f7be59984
kosamega <kitchonkosamega@gmail.com>
2023-01-20 14:41:14 +0900

    rubocop

--- test/integration/artists_create_test.rb
+++ test/integration/artists_create_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ArtistsCreateTest < ActionDispatch::IntegrationTest
   def setup
@@ -7,11 +7,11 @@ class ArtistsCreateTest < ActionDispatch::IntegrationTest
 
   test 'musicsのnewでartistをajaxで正常に追加。データリストにartistが正常に追加。' do
     get new_album_music_path(@album)
-    assert_select "option[value=new_artist]", false
+    assert_select 'option[value=new_artist]', false
     assert_difference 'Artist.count', 1 do
-      post artists_path, xhr: true, params: {artist: {name: "new_artist"}}
+      post artists_path, xhr: true, params: { artist: { name: 'new_artist' } }
     end
     assert_template 'artists/create'
-    assert_select "option[value=new_artist]"
+    assert_select 'option[value=new_artist]'
   end
 end
--- test/integration/artists_edit_test.rb
+++ test/integration/artists_edit_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ArtistsEditTest < ActionDispatch::IntegrationTest
   def setup
@@ -10,17 +10,17 @@ class ArtistsEditTest < ActionDispatch::IntegrationTest
   test 'artistの編集に失敗' do
     get edit_artist_path(@artist)
     assert_template 'artists/edit'
-    patch artist_path(@artist), params: {artist: {name: ""}}
+    patch artist_path(@artist), params: { artist: { name: '' } }
     assert_redirected_to edit_artist_path(@artist)
   end
 
   test 'artistの編集に成功' do
     get edit_artist_path(@artist)
     assert_template 'artists/edit'
-    patch artist_path(@artist), params: {artist: {name: "new_artist"}}
+    patch artist_path(@artist), params: { artist: { name: 'new_artist' } }
     assert flash.any?
     assert_redirected_to artist_path(@artist)
     @artist.reload
-    assert_equal "new_artist", @artist.name
+    assert_equal 'new_artist', @artist.name
   end
 end
