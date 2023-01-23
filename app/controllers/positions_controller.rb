dce6a3636ffb43410b74b5f34329959946d13783
kosamega <kitchonkosamega@gmail.com>
2023-01-23 21:30:27 +0900

    correct_userを追加

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,9 +1,17 @@
 class PositionsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :correct_user
+
   def update
     return if params[:drag] == params[:drop]
 
     MusicPlaylistRelation.find_by(playlist_id: params[:playlist_id],
                                   position: params[:drag]).insert_at(params[:drop].to_i)
+  end
+
+  private
+
+  def correct_user
+    redirect_to '/' unless current_user == Playlist.find(params[:playlist_id]).user
   end
 end
