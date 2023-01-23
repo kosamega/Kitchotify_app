3ef33cc6b1a188a4fe2cd3d70ea43d8bddb33e79
kosamega <kitchonkosamega@gmail.com>
2023-01-23 21:32:31 +0900

    positions_controllerのテスト関連を修正

--- test/models/music_playlist_relation_test.rb
+++ test/models/music_playlist_relation_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MusicPlaylistRelationTest < ActiveSupport::TestCase
   def setup
-    @relation = MusicPlaylistRelation.new(playlist_id: playlists(:one).id, music_id: musics(:music1).id)
+    @relation = MusicPlaylistRelation.new(playlist_id: playlists(:playlist1).id, music_id: musics(:music1).id)
   end
 
   test 'relationが有効' do
