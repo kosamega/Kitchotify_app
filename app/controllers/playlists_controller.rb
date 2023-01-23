17214ff46e970eedf3afcba6d930aae8d04ebd85
kosamega <kitchonkosamega@gmail.com>
2023-01-23 17:05:29 +0900

    n+1問題を解消

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -14,7 +14,7 @@ class PlaylistsController < ApplicationController
   def show
     @at_playlist_show = true
     @relations = @playlist.music_playlist_relations.sort_by { |a| a[:position] }
-    @musics = @playlist.included_musics
+    @musics = @playlist.included_musics.includes(:artist, album: [jacket_attachment: :blob], audio_attachment: :blob)
     @infos = set_infos(@musics)
     gon.infos_j = @infos
     gon.playlist_id = @playlist.id
