3dc8108088a24c1cdffcb2a648b67fbbbaf5c1a8
kosamega <kitchonkosamega@gmail.com>
2023-01-23 21:32:44 +0900

    n+1を修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   before_action :logged_in_user
 
   def home
-    @albums_released = Album.where(released: true).with_attached_jacket
+    @albums_released = Album.where(released: true).includes(jacket_attachment: :blob)
     @comments = Comment.limit(10).includes([music: :artist], :user, [album: [jacket_attachment: :blob]])
     @playlists = Playlist.where(public: true).includes(:user)[0..9]
   end
