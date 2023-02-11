35a26eb1159aa9dea230c810956faf231360ea41
kosamega <kitchonkosamega@gmail.com>
2023-02-11 13:32:10 +0900

    AccessKeyをapi_keyに変更

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -3,12 +3,12 @@ class Api::Base < ApplicationController
   before_action :Authenticate
   
   def parameter_exist?
-    return render json: { status: '400', error: 'nameとAccessKeyが足りません' } if params[:name].nil? && params[:AccessKey].nil?
+    return render json: { status: '400', error: 'nameとapi_keyが足りません' } if params[:name].nil? && params[:api_key].nil?
     return render json: { status: '400', error: 'nameが足りません' } if params[:name].nil?
-    return render json: { status: '400', error: 'AccessKeyが足りません' } if params[:AccessKey].nil?
+    return render json: { status: '400', error: 'api_keyが足りません' } if params[:api_key].nil?
   end
     
   def Authenticate
-    return render json: { status: '401', error: 'AccessKeyが間違っています' } if params[:AccessKey] != ENV.fetch('KITCHOTIFY_API_KEY')
+    return render json: { status: '401', error: 'api_keyが間違っています' } if params[:api_key] != ENV.fetch('KITCHOTIFY_API_KEY')
   end
 end
