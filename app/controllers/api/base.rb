2b79e7cac602deefd0561faf5b1d7d13134a5c8c
kosamega <kitchonkosamega@gmail.com>
2023-02-11 17:10:26 +0900

    rubocop

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,21 +1,21 @@
 class Api::Base < ApplicationController
   before_action :authenticate
   before_action :parameter_exist?
-  
+
   def authenticate
     authenticate_api_token || render_unauthorized
   end
-  
+
   def authenticate_api_token
     authenticate_with_http_token do |token|
       token == ENV.fetch('KITCHOTIFY_API_TOKEN')
     end
   end
-  
+
   def render_unauthorized
     render json: { message: 'tokenが間違っています' }, status: :unauthorized
   end
-  
+
   def parameter_exist?
     return render json: { error: 'nameが足りません' }, status: :bad_request if params[:name].nil?
   end
