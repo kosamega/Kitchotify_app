8a8a283209f0a8fa518d42776794266866cc238d
kosamega <kitchonkosamega@gmail.com>
2023-02-13 20:39:35 +0900

    introquizにvalidationを追加

--- app/models/intro_quiz.rb
+++ app/models/intro_quiz.rb
@@ -1,6 +1,6 @@
 class IntroQuiz < ApplicationRecord
   has_many :quiz_results
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :range, presence: true
   validates :q_num, presence: true
 end
