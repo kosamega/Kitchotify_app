536ad0336ffc32493e588408ae08a96cf72b1a65
kosamega <kitchonkosamega@gmail.com>
2023-02-13 20:48:01 +0900

    music_playlist_relationのvalidationを追加

--- app/models/music_playlist_relation.rb
+++ app/models/music_playlist_relation.rb
@@ -2,4 +2,5 @@ class MusicPlaylistRelation < ApplicationRecord
   belongs_to :playlist
   belongs_to :music
   acts_as_list scope: :playlist, top_of_list: 0
+  validates :position, uniqueness: { scope: :playlist_id }
 end
