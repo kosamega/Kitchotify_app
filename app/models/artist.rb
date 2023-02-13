dc8c716505dc7e3d5030332ea4139b5c904ee4f1
kosamega <kitchonkosamega@gmail.com>
2023-02-13 20:31:16 +0900

    artistのvalidationを追加

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ApplicationRecord
   # belongs_to :user
   has_many :musics, -> { order(id: :desc) }
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   default_scope -> { order(id: :desc) }
 end
