c79d7c705dda016f3f585a1a1e1ba585f2d30af6
kosamega <kitchonkosamega@gmail.com>
2023-02-15 18:05:36 +0900

    検索APIのテスト

---
+++ test/controllers/api/albums_controller_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class Api::AlbumsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @album = albums(:album1)
+  end 
+
+  test "tokenが不正な場合401を返す" do
+    get api_albums_path , params: {name: @album.name}
+    assert_response 401
+    res = JSON.parse(response.body)
+    assert_equal ['tokenが間違っています'], res['messages']
+  end
+
+  test 'パラメータにnameが足らないときは400を返す' do
+    get api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_API_TOKEN')}" }
+    assert_response 400
+    res = JSON.parse(response.body)
+    assert_equal 'nameが足りません', res['messages']
+  end
+
+  test '成功したときは200を返す' do
+    get api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_API_TOKEN')}" }, params: {name: @album.name}
+    assert_response 200
+    res = JSON.parse(response.body)
+    assert_equal @album.name, res['albums'][0]['name']
+    assert_equal @album.kiki_taikai_date, res['albums'][0]['kiki_taikai_date']
+    assert_equal album_url(@album), res['albums'][0]['url']
+  end
+end
---
+++ test/controllers/api/artists_controller_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class ArtistsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @artist = artists(:artist1)
+  end 
+  
+  test "tokenが不正な場合401を返す" do
+    get api_artists_path , params: {name: @artist.name}
+    assert_response 401
+    res = JSON.parse(response.body)
+    assert_equal ['tokenが間違っています'], res['messages']
+  end
+
+  test 'パラメータにnameが足らないときは400を返す' do
+    get api_artists_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_API_TOKEN')}" }
+    assert_response 400
+    res = JSON.parse(response.body)
+    assert_equal 'nameが足りません', res['messages']
+  end
+
+  test '成功したときは200を返す' do
+    get api_artists_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_API_TOKEN')}" }, params: {name: @artist.name}
+    assert_response 200
+    res = JSON.parse(response.body)
+    assert_equal @artist.name, res['artists'][0]['name']
+    assert_equal @artist.bio, res['artists'][0]['bio']
+    assert_equal artist_url(@artist), res['artists'][0]['url']
+  end
+end
