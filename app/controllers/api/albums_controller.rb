6a9d7e5fedafb7cc9302bd3203f2f3a63a7ce550
kosamega <kitchonkosamega@gmail.com>
2023-02-15 17:14:02 +0900

    createを追加

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,5 +1,17 @@
 class Api::AlbumsController < Api::Base
+  before_action :name_exist?, only: %i[index]
+
   def index
     @albums = Album.where('UPPER(name) LIKE ?', "%#{params[:name].upcase}%").includes(musics: %i[artist album])
+  end
+
+  def create
+    @album = Album.new(name: params[:name], kiki_taikai_date: params[:kiki_taikai_date])
+    if @album.save
+      @album
+    else
+      @messages = @album.errors.full_messages
+      render 'api/shared/errors', status: :conflict
+    end
   end
 end
