f52010c7eec25d2e0e1b9810d3f6aa5bd610011c
kosamega <kitchonkosamega@gmail.com>
2023-02-15 18:41:48 +0900

    rubocop

--- test/integration/api/albums_create_api_test.rb
+++ test/integration/api/albums_create_api_test.rb
@@ -3,39 +3,43 @@ require 'test_helper'
 class Api::AlbumsCreateApiTest < ActionDispatch::IntegrationTest
   def setup
     @album = albums(:album1)
-  end 
+  end
 
-  test "tokenが不正な場合401を返す" do
-    post api_albums_path , params: {name: 'new_album', kiki_taikai_date: 2099-01-01}
+  test 'tokenが不正な場合401を返す' do
+    post api_albums_path, params: { name: 'new_album', kiki_taikai_date: 2099 - 0o1 - 0o1 }
     assert_response 401
     res = JSON.parse(response.body)
     assert_equal ['tokenが間違っています'], res['messages']
   end
 
   test 'パラメータにnameが足らないときは400を返す' do
-    post api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" }, params: {kiki_taikai_date: 2099-01-01}
+    post api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" },
+                          params: { kiki_taikai_date: 2099 - 0o1 - 0o1 }
     assert_response 400
     res = JSON.parse(response.body)
     assert_equal ['nameが足りません'], res['messages']
   end
 
   test 'パラメータにkiki_taikai_dateが足らないときは400を返す' do
-    post api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" }, params: {name: 'new_album'}
+    post api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" },
+                          params: { name: 'new_album' }
     assert_response 400
     res = JSON.parse(response.body)
     assert_equal ['kiki_taikai_dateが足りません'], res['messages']
   end
 
   test '同名のアルバムが存在している場合409を返す' do
-    post api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" }, params: {name: @album.name, kiki_taikai_date: 2099-01-01}
+    post api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" },
+                          params: { name: @album.name, kiki_taikai_date: 2099 - 0o1 - 0o1 }
     assert_response 409
     res = JSON.parse(response.body)
-    assert_equal ["Nameはすでに存在します"], res['messages']
+    assert_equal ['Nameはすでに存在します'], res['messages']
   end
 
   test '成功したときは200を返す' do
     assert_difference 'Album.count', 1 do
-      post api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" }, params: {name: 'new_album', kiki_taikai_date: 2099-01-01}
+      post api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" },
+                            params: { name: 'new_album', kiki_taikai_date: 2099 - 0o1 - 0o1 }
     end
     assert_response 200
     @new_album = Album.find_by(name: 'new_album')
--- test/integration/api/albums_search_api_test.rb
+++ test/integration/api/albums_search_api_test.rb
@@ -3,24 +3,25 @@ require 'test_helper'
 class Api::AlbumsSearchApiTest < ActionDispatch::IntegrationTest
   def setup
     @album = albums(:album1)
-  end 
+  end
 
-  test "tokenが不正な場合401を返す" do
-    get api_albums_path , params: {name: @album.name}
+  test 'tokenが不正な場合401を返す' do
+    get api_albums_path, params: { name: @album.name }
     assert_response 401
     res = JSON.parse(response.body)
     assert_equal ['tokenが間違っています'], res['messages']
   end
 
   test 'パラメータにnameが足らないときは400を返す' do
-    get api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_SEARCH_API_TOKEN')}" }
+    get api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_SEARCH_API_TOKEN')}" }
     assert_response 400
     res = JSON.parse(response.body)
     assert_equal ['nameが足りません'], res['messages']
   end
 
   test '成功したときは200を返す' do
-    get api_albums_path, headers: { 'Authorization': "Bearer #{ENV.fetch('KITCHOTIFY_SEARCH_API_TOKEN')}" }, params: {name: @album.name}
+    get api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_SEARCH_API_TOKEN')}" },
+                         params: { name: @album.name }
     assert_response 200
     res = JSON.parse(response.body)
     assert_equal @album.name, res['albums'][0]['name']
