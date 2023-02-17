84536681b9c17348fd5b6ecc18ee3094e8365a0e
kosamega <kitchonkosamega@gmail.com>
2023-02-17 22:56:18 +0900

    musicにテストを追加

--- test/models/music_test.rb
+++ test/models/music_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class MusicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @music = musics(:music1)
+  end
+
+  test 'musicが有効' do
+    @music.valid?
+  end
+
+  test 'nameは空でない' do
+    @music.name = ''
+    assert_not @music.valid?
+  end
+
+  test 'nameは100文字以下' do
+    @music.name = 'a' * 101
+    assert_not @music.valid?
+  end
+
+  test 'trackは1000より小さい' do
+    @music.track = 1000
+    assert_not @music.valid?
+  end
 end
