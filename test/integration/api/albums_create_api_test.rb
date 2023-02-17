38338c9a318262c5385c561248a78f77409183cf
kosamega <kitchonkosamega@gmail.com>
2023-02-17 20:23:26 +0900

    kiki_taikai_date関連のテストを修正

--- test/integration/api/albums_create_api_test.rb
+++ test/integration/api/albums_create_api_test.rb
@@ -6,7 +6,7 @@ class Api::AlbumsCreateApiTest < ActionDispatch::IntegrationTest
   end
 
   test 'tokenが不正な場合401を返す' do
-    post api_albums_path, params: { name: 'new_album', kiki_taikai_date: 2099 - 0o1 - 0o1 }
+    post api_albums_path, params: { name: 'new_album', kiki_taikai_date: '2099-01-01' }
     assert_response 401
     res = JSON.parse(response.body)
     assert_equal ['tokenが間違っています'], res['messages']
@@ -14,7 +14,7 @@ class Api::AlbumsCreateApiTest < ActionDispatch::IntegrationTest
 
   test 'パラメータにnameが足らないときは400を返す' do
     post api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" },
-                          params: { kiki_taikai_date: 2099 - 0o1 - 0o1 }
+                          params: { kiki_taikai_date: '2099-01-01' }
     assert_response 400
     res = JSON.parse(response.body)
     assert_equal ['nameが足りません'], res['messages']
@@ -30,22 +30,22 @@ class Api::AlbumsCreateApiTest < ActionDispatch::IntegrationTest
 
   test '同名のアルバムが存在している場合409を返す' do
     post api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" },
-                          params: { name: @album.name, kiki_taikai_date: 2099 - 0o1 - 0o1 }
+                          params: { name: @album.name, kiki_taikai_date: '2099-01-01' }
     assert_response 409
     res = JSON.parse(response.body)
-    assert_equal ['Nameはすでに存在します'], res['messages']
+    assert_equal ["Nameはすでに存在します"], res['messages']
   end
 
   test '成功したときは200を返す' do
     assert_difference 'Album.count', 1 do
       post api_albums_path, headers: { Authorization: "Bearer #{ENV.fetch('KITCHOTIFY_CREATE_API_TOKEN')}" },
-                            params: { name: 'new_album', kiki_taikai_date: 2099 - 0o1 - 0o1 }
+                            params: { name: 'new_album', kiki_taikai_date: '2099-01-01' }
     end
     assert_response 200
     @new_album = Album.find_by(name: 'new_album')
     res = JSON.parse(response.body)
     assert_equal @new_album.name, res['album']['name']
-    assert_equal @new_album.kiki_taikai_date, res['album']['kiki_taikai_date']
+    assert_equal @new_album.kiki_taikai_date.strftime("%Y-%m-%d"), res['album']['kiki_taikai_date']
     assert_equal new_album_music_url(@new_album), res['album']['form_url']
   end
 end
