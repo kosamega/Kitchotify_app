68242d9c1b2a820cb49000e4c9a06e90cd1f06cd
kosamega <kitchonkosamega@gmail.com>
2023-03-24 12:38:19 +0900

    jsのディレクトリを変更

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,14 +1,4 @@
 //= link_tree ../images
 //= link_tree ../audio
 //= link_tree ../javascripts
-//= link side_bar.js
-//= link audio_player.js
-//= link playlist.js
 //= link application.css
-//= link new_playlist.js
-//= link edit_playlist.js
-//= link intro_quiz.js
-//= link music_new.js
-//= link copy_link.js
-//= link like.js
-//= link get_csrf_token.js
