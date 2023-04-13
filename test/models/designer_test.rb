7b5a901cb8ad1ceedda0e6ca0a01dca01167802b
kosamega <kitchonkosamega@gmail.com>
2023-04-13 12:08:21 +0900

    designerのvalidationとテストを追加

--- test/models/designer_test.rb
+++ test/models/designer_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class DesignerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @designer = designers(:designer1)
+  end
+
+  test "名前が一意" do
+    duplicate_designer = @designer.dup
+    assert_not duplicate_designer.valid?
+  end
+
+  test "名前が空でない" do
+    @designer.name = ""
+    assert_not @designer.valid?
+  end
 end
