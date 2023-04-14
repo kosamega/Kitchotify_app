0d912e9cd17e83b399d8a9942a6c6d6b0b33f775
kosamega <kitchonkosamega@gmail.com>
2023-04-14 17:30:15 +0900

    フロントエンドを分離

--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,33 @@ Rails.application.routes.draw do
     resources :musics, only: %i[index]
     resources :artists, only: %i[index]
   end
+
+  namespace :backend_api, defaults: {format: :json}do
+    resources :albums do
+      resources :musics, only: %i[new create edit update show destroy]
+      resource :index, only: %i[create]
+      resource :music_list, only: %i[create]
+    end
+
+    resources :artists
+
+    resources :comments, only: %i[create]
+
+    resources :likes, only: %i[create destroy index]
+
+    resources :music_playlist_relations, only: %i[create destroy]
+    resources :playlists, only: %i[create update destroy index show] do
+      resource :position, only: %i[update]
+    end
+
+    resource :sessions, only: %i[new create destroy show]
+    resource :users, only: %i[new create show edit update index]
+
+    resources :intro_quizzes, only: %i[index show new]
+    resources :quiz_results, only: %i[create show index]
+
+    resources :searches, only: %i[index]
+
+    resource :import_csvs, only: %i[new show create]
+  end
 end
