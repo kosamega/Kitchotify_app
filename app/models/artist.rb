ce815b08f50b60682cb3fd1c0eb5c0837ba00629
kosamega <kitchonkosamega@gmail.com>
2023-04-16 20:41:22 +0900

    belongs_to :userを追加

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ApplicationRecord
   # belongs_to :user
   has_many :musics, -> { order(id: :desc) }
+  belongs_to :user, optional: true
   validates :name, presence: true, uniqueness: true
   default_scope -> { order(id: :desc) }
 end
