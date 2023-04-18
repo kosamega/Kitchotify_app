c1e8a8a3c4e380f92118fa1c6f1eef22e5753c00
kosamega <kitchonkosamega@gmail.com>
2023-04-18 12:28:46 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_110237) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_032818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -128,6 +128,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_110237) do
     t.integer "user_id"
     t.integer "intro_quiz_id"
     t.integer "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_artist_ownerships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
