00b1cdf90b5d16aa82b343fd3eddb9ac007ff9f0
kosamega <kitchonkosamega@gmail.com>
2023-04-26 23:14:58 +0900

    userとartistの組が一意

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_032818) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_26_133158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -137,6 +137,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_032818) do
     t.integer "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_user_artist_ownerships_on_artist_id"
+    t.index ["user_id", "artist_id"], name: "index_user_artist_ownerships_on_user_id_and_artist_id", unique: true
+    t.index ["user_id"], name: "index_user_artist_ownerships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
