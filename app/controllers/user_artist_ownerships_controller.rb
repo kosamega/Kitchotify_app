cedad2f1e6e6ee4c68447b65081867ac6cb38186
kosamega <kitchonkosamega@gmail.com>
2023-04-26 23:23:53 +0900

    controller

--- app/controllers/user_artist_ownerships_controller.rb
+++ app/controllers/user_artist_ownerships_controller.rb
@@ -1,17 +1,37 @@
 class UserArtistOwnershipsController < ApplicationController
   before_action :logged_in_user
-  before_action :set_user_artist_ownership, only: %i[destroy]
+  before_action :set_ownership, only: %i[destroy]
 
   def create
-    UserArtistOwnership.create(user_id: params[:user_id], artist_id: params[:artist_id])
+    @ownership = UserArtistOwnership.new(ownership_params)
+    if @ownership.save
+      @save = true
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
+    else
+      @save = false
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.js
+      end
+    end
   end
 
   def destroy
-    @user_artist_ownership.destroy
+    user = @ownership.user
+    artist = @ownership.artist 
+    @ownership.destroy
+    render json: {message: "#{user.name}と#{artist.name}の紐付けを削除しました", user: user.name, user_id: user.id}
   end
 
   private
-  def set_user_artist_ownership
-    @user_artist_ownership = UserArtistOwnership.find(prams[:id])
+  def set_ownership
+    @ownership = UserArtistOwnership.find(params[:id])
+  end
+
+  def ownership_params
+    params.require(:user_artist_ownership).permit(:user_id, :artist_id)
   end
 end
