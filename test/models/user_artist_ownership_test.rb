b6c0b1595784f869aef9aa94910049cd6831b7c6
kosamega <kitchonkosamega@gmail.com>
2023-04-26 23:18:39 +0900

    ownershipのテスト

--- test/models/user_artist_ownership_test.rb
+++ test/models/user_artist_ownership_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UserArtistOwnershipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @ownership = user_artist_ownerships(:one)
+  end
+
+  test "ownershipが有効" do
+    assert @ownership.valid?
+  end
+
+  test '同一userを同一artistに追加できない' do
+    duplicate_ownership = @ownership.dup
+    assert_not duplicate_ownership.valid?
+  end
 end
