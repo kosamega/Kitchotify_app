1f4978f083354def2116417380453854a8dd9dec
kosamega <kitchonkosamega@gmail.com>
2023-05-07 23:17:05 +0900

    fixtureの変更を反映

--- test/controllers/daikichis_controller_test.rb
+++ test/controllers/daikichis_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class DaikichisControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @daikichi = daikichis(:one)
+    @daikichi = daikichis(:daikichi1)
     @admin_user = users(:admin_user)
   end
 
@@ -16,8 +16,8 @@ class DaikichisControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@admin_user)
     assert_difference('Daikichi.count') do
       post daikichis_url,
-           params: { daikichi: { counting_votes_date: @daikichi.counting_votes_date, designer_id: @daikichi.designer_id,
-                                 name: @daikichi.name, released: @daikichi.released } }
+           params: { daikichi: { counting_votes_date: '2099-01-01', designer_id: @daikichi.designer_id,
+                                 name: '新規大吉', released: false } }
     end
 
     assert_redirected_to daikichi_url(Daikichi.last)
