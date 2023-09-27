54c065da1d20d2a8efac633cb63da4a7bef3a58f
kosamega <kitchonkosamega@gmail.com>
2023-09-27 23:01:50 +0900

    ログインしたらフラッシュ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name])
     if user&.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = 'ログインしました'
       return redirect_back_or root_path if user.role_kitchonkun?
 
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
